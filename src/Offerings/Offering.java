a9b8d184fae5bf5a87ebb233c259389d39fa3f1e
Abdelrahman <a_a6282@live.concordia.ca>
2024-10-18 23:57:11 -0400

    Added classes to Offering package

---
+++ src/Offerings/Offering.java
@@ -0,0 +1,15 @@
+public class Offering {
+    private Lesson lesson;
+
+    public Offering(Lesson lesson) {
+        this.lesson = lesson;
+    }
+
+    public Lesson getLesson() {
+        return lesson;
+    }
+
+    public void setLesson(Lesson lesson) {
+        this.lesson = lesson;
+    }
+}
